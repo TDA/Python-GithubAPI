092349631fde091084c849d4ad745f5e13b4de03
Sai Pc <schand31@asu.edu>
2015-12-27 20:19:11 +0530

    Listing any users repo

--- list_repos.py
+++ list_repos.py
@@ -1,11 +1,19 @@
 __author__ = 'saipc'
 
 import requests
+
+data_store = 'my_repo_data.json'
 # enter password here
+# get my repo
 r = requests.get("https://api.github.com/user/repos", auth=('tda', ''))
 print r.status_code
 
+# get repos of any user
+username = 'tda'
+r = requests.get("https://api.github.com/users/" + username + "/repos", auth=('tda', ''))
+
+print r.status_code
 print r.text
 
-with open('my_repo_data.json', 'a') as file:
+with open(data_store, 'a') as file:
     file.write(r.text)
